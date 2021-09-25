75af6a0f3889da6df506f175fcdd8926d2cfdd5f
vijayrajan23 <vijayrajan23@gmail.com>
2021-09-25 14:53:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
 variable "region" {
     default = "us-east-1"  
 }
+
+variable "key-public" {
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClnTD+83/yLz4YgFdXJw8CEIxYGMtuPhTf7dczGsxHUqovEbdRRgORNcPfG4U8Fdsu4gRTRm+re4ds/GELyu3QHslPLtNnfl7CwxotLDifyl7YN5JtcCEuopZRXWBwha4DwuvDmgzQvj8UlhQIuIjfQ1rIyim+6cFSGsXC38Vr9o/yeqRHehfNxyu9kWl8HPzDfqDmqvod4pTzWtkFw0H3JT0S1eVNSBMGr4v6mz6sOYqc9lMLVl944xqoXyV/FGip9UtQ4n0XpaDPcqjfCrLNKux6hqtuQ/Wzdeqyii1fzV45JclU917qytyvl+vIHQv64pFfl7TZzALLuk9VwqFZ rits07@rits07-HP-Laptop-15-da0xxx"
+  
+}
